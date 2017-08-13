7171f09f138e3e092ac1e05e41fd35e1d700178d
Kamal-Singh <ksinghyadav@ymail.com>
2017-08-13 11:10:33 +0530

    Update dict.py
    Changed levenshtein distance algorithm to LCS(longest common subsequence) and also added functionality for multiple choices if entered word is wrong.

--- dict.py
+++ dict.py
@@ -4,39 +4,41 @@ def edit_diff(str1,str2,m,n):
     dp = [[0 for x in range(n+1)] for y in range(m+1)]
     for i in range(m+1):
         for j in range(n+1):
-            if i==0:
-                dp[i][j]=j
-            elif j==0:
-                dp[i][j]=i
+            if i==0 or j==0:
+                dp[i][j]=0
             elif str1[i-1]==str2[j-1]:
-                dp[i][j]=dp[i-1][j-1]
+                dp[i][j]=dp[i-1][j-1]+1
             else:
-                dp[i][j]=1+min(dp[i][j-1],dp[i-1][j-1],dp[i-1][j])
+                dp[i][j]=max(dp[i][j-1],dp[i-1][j])
     return dp[m][n]
 
 def best_match(str1,keys):
-    min=1001
+    m=0
+    bmatch=[]
     for str2 in keys:
         diff=edit_diff(str1,str2,len(str1),len(str2))
-        if(diff<min):
-            min=diff
-            best_match=str2
-    return best_match
+        if(diff>=m and len(str2)<=len(str1)):
+            m=diff
+            bmatch.insert(0,str2)
+    return bmatch
 
 def search(str1,data):
     str1=str1.lower()
     if str1 in data:
         return data[str1]
-    elif best_match(str1,list(data.keys())):
+    elif len(best_match(str1,list(data.keys())))>0:
         match=best_match(str1,data.keys())
-        print('Do you mean %s ? If yes enter "Y" else "N" ' %match)
-        yn=input()
-        if yn=='Y' or yn=='y':
-            return data[match]
-        elif yn=='N' or yn=='n':
-            return 'The entered word doesn\'t exist, Double check it.'
+        for word in match:
+            print('Do you mean %s ? If yes enter "Y" else "N" ' %word)
+            yn=input()
+            if yn=='Y' or yn=='y':
+                return data[word]
+            elif yn=='N' or yn=='n':
+                continue
+            else:
+                return 'The entered Query Doesn\'t exist'
         else:
-            return 'The entered Query Doesn\'t exist'
+            return 'The entered word doesn\'t exist, Double check it.'
     else:
         return 'The entered word doesn\'t exist, Double check it.'
 
@@ -48,6 +50,3 @@ if type(ans)==list:
         print(i)
 else:
     print(ans)
-
-
-
